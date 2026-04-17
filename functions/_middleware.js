0076b57feee419e961737073d067e9eecb5251a3
Rohan Mehta <rohan.mehta@theceomagazine.com>
2026-04-17 15:12:54 +0530

    kv

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,28 +1,17 @@
 export async function onRequest(context) {
-  const { request, env, next } = context;
+  const { env } = context;
 
-  let response = await next();
+  try {
+    const header = await env.TEMPLATES.get("header");
 
-  if (!response.headers.get("content-type")?.includes("text/html")) {
-    return response;
+    return new Response(
+      header ? "KV WORKING" : "KV EMPTY",
+      { status: 200 }
+    );
+  } catch (e) {
+    return new Response(
+      "KV ERROR: " + e.message,
+      { status: 500 }
+    );
   }
-
-  const header = await env.TEMPLATES.get("header");
-
-  return new Response(header || "KV NOT WORKING");
-  
-  const footer = await env.TEMPLATES.get("footer");
-
-  return new HTMLRewriter()
-    .on('#header', {
-      element(el) {
-        el.setInnerContent(header || '', { html: true });
-      }
-    })
-    .on('#footer', {
-      element(el) {
-        el.setInnerContent(footer || '', { html: true });
-      }
-    })
-    .transform(response);
 }
