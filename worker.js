9f568c4ee0d2abb16893110a47908db90af0f62d
Rohan Mehta <rohan.mehta@theceomagazine.com>
2026-04-20 16:58:07 +0530

    blog

--- worker.js
+++ worker.js
@@ -5,6 +5,7 @@ const MENU_HTML = `
     <li><a href="index.html">Home</a></li>
     <li><a href="about.html">About Us</a></li>
     <li><a href="services.html">Solutions</a></li>
+    <li><a href="blog">Blog</a></li>
     <li><a href="contact.html">Contact Us</a></li>
   </ul>
 </nav>
